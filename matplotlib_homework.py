b05e0be66f44ad076fad8698efb2c54fd7fc7179
NongCz <Boonlerd2400@gmail.com>
2025-01-23 10:16:49 +0700

    Third task

--- matplotlib_homework.py
+++ matplotlib_homework.py
@@ -8,9 +8,9 @@ y2 = x * np.sin(2 * x)
 y3 = np.arctan(x)
 
 plt.figure(figsize=(10, 6))
-plt.plot(x, y1, label="f(x) = x^2")
-plt.plot(x, y2, label="f(x) = x * sin(2x)")
-plt.plot(x, y3, label="f(x) = arctan(x)")
+plt.plot(x, y1, label="f(x) = x^2", color="green")  
+plt.plot(x, y2, label="f(x) = x * sin(2x)", color="red")  
+plt.plot(x, y3, label="f(x) = arctan(x)", color="purple")  
 
 plt.title("Graphs of Functions")
 plt.xlabel("x-axis")
